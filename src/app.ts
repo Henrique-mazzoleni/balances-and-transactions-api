397e6030b16d2223ff4a6663b666c51382dcc7e1
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-09-09 09:42:29 +0200

    access to api established and necessary data retrieved

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,46 @@
-import "dotenv/config";
-import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json";
-import { getHistoricalBalance } from "./services/getHistoricalBalances";
+import 'dotenv/config';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+import {
+  getHistoricalBalance,
+  getBalance,
+  getTransactions,
+} from './services/getHistoricalBalances';
+
+type BalanceData = {
+  amount: number;
+  currency: string;
+  date: string;
+};
+
+type TransactionData = {
+  amount: number;
+  currency: string;
+  date: string;
+  status: string;
+};
 
 const app = express();
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/historical-balances", (req, res) => {
-  const historicalBalance = getHistoricalBalance();
-  return res.json(historicalBalance);
+app.get('/historical-balances', async (req, res) => {
+  try {
+    const apiKey = req.get('x-api-key');
+    if (apiKey) {
+      const currentBalance: BalanceData = await getBalance(apiKey);
+      const transactions: TransactionData[] = await getTransactions(apiKey);
+      console.log(currentBalance);
+      console.log(transactions);
+    } else {
+      throw Error('API key missing.');
+    }
+
+    const historicalBalance = getHistoricalBalance();
+    return res.json(historicalBalance);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export default app;
--- src/services/getHistoricalBalances.ts
+++ src/services/getHistoricalBalances.ts
@@ -1,5 +1,35 @@
-// This is just an example, it is not expected from you to keep this function
-// signature or even the file name. Feel free to change whatever you like.
+import axios from 'axios';
+
 export function getHistoricalBalance() {
-  return { hello: "world" };
+  return { hello: 'world' };
+}
+
+export async function getBalance(apiKey: string) {
+  try {
+    if (process.env.API_URL) {
+      const { data } = await axios.get(process.env.API_URL + '/balances', {
+        headers: { 'x-api-key': apiKey },
+      });
+      return data;
+    } else {
+      throw new Error('API URL missing.');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getTransactions(apiKey: string) {
+  try {
+    if (process.env.API_URL) {
+      const { data } = await axios.get(process.env.API_URL + '/transactions', {
+        headers: { 'x-api-key': apiKey },
+      });
+      return data;
+    } else {
+      throw new Error('API URL missing.');
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
